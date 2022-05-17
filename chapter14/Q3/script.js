9b7b0da5753bfbaafcba409612a3f4bbae8fe998
Luis Ballote <ealjkl@gmail.com>
2022-05-17 04:34:56 -0500

    Fixing 5.3

--- chapter14/Q3/script.js
+++ chapter14/Q3/script.js
@@ -1,108 +1,98 @@
-//I totally forgot that document.body existed
-let body = document.body;
-let bodyParent = body.parentElement;
-let firstRendered = false;
+let temp;
+const template1 = document.getElementById("template-list-item");
+const template2 = document.getElementById("template-item-full");
+const currentPageElement = document.getElementById("current-page");
+const page1 = new DocumentFragment();
+const countriesUl = document.getElementById("countries");
 
-let template1 = document.getElementById("template-list-item");
-let template2 = document.getElementById("template-item-full");
+let currentPage;
+let renderedPage1 = false; 
+let renderedAnyPage = false;
+
 
 fetch("./data/countries.json")
     .then(response => response.json())
-    .then(items => {
-        renderPage(items);
+    .then(countries => {
+        renderPage(countries);
     });
 
-
-function renderPage(items) {
+function renderPage(countries, shallPushState=true) {
     const params = new Proxy(new URLSearchParams(window.location.search), 
         {
             get: (searchParams, prop) => searchParams.get(prop),
         }
     );
-    if (params.key) {
-        renderTemplate2(items, params.key)
-    } else {
-        renderTemplate1(items);
-        firstRendered = true;
-    }
-    window.addEventListener("popstate", e => {
-        renderPage(items);
-    })
-}
 
-function renderTemplate1(items) {
-    if (!firstRendered) {
-        firstRenderTemplate1(items);
+    const {country} = params;  
+    if (country == null) {
+        renderPage1(countries, shallPushState);
     } else {
-        reRenderTemplate1();
+        renderPage2(countries, country, shallPushState);
+    }
+
+    if (!renderedAnyPage) {
+        window.addEventListener("popstate", () => renderPage(countries, false));
+        renderedAnyPage = true;
     }
 }
 
-function firstRenderTemplate1(items) {
-    let currentBody = bodyParent.querySelector("body");
-    if (currentBody !== body) bodyParent.replaceChild(body, currentBody);
-    let countries = body.querySelector("#countries");
-    for (let key in items) {
-        let item = items[key];
-        let { country, text, imagePath } = item;
-        let fragment = template1.content.cloneNode(true);
+function renderPage1(countries, shallPushState=true) {
+    if (shallPushState) history.pushState(null, null, "./index.html"); 
+    if (!renderedPage1) {
+        for (const countryKey in countries) {
+            const country = countries[countryKey];
+            const {name, text, imagePath} = country;
+            const countryLi = template1.cloneNode(true).content.querySelector("li");
+            const itemImage = countryLi.querySelector(".item-side-image");
+            const itemBody = countryLi.querySelector(".item-body");
+            const itemTitle = itemBody.querySelector(".item-title");
+            const itemText = itemBody.querySelector(".item-text");
 
-        let li = fragment.querySelector("li");
-        li.setAttribute("data-key", key);
+            countryLi.setAttribute("data-key", countryKey);
+            itemTitle.textContent = name;
+            itemText.textContent = text;
+            itemImage.src = imagePath;
+            countriesUl.appendChild(countryLi);
 
-        let h2 = fragment.querySelector("h2");
-        h2.textContent = country; 
-    
-        let img = fragment.querySelector("img");
-        img.src = imagePath;
-    
-        let p = fragment.querySelector("p");
-    
-        let maxChars = 400;
-        let extra = text.length > maxChars ? "...": "";
-        p.textContent = text.slice(0, maxChars) + extra; 
-        
-        countries.appendChild(li);
-    }
-
-    countries.addEventListener("click", evt => {
-        let target = evt.target;
-        let li = target.closest("li");
-        if (li) {
-            renderTemplate2(items, li.getAttribute("data-key"));
         }
-    }); 
-    firstRendered = true;
+        countriesUl.addEventListener("click", e => {
+            const li = e.target.closest("li"); 
+            if (li) {
+                const countryKey = li.getAttribute("data-key");
+                renderPage2(countries, countryKey);
+            }
+        });
+        renderedPage1 = true;
+    } 
+    if (currentPage === "page2") {
+        currentPageElement.replaceChildren(page1);
+    } 
+    currentPage = "page1";
 }
 
-function renderTemplate2(items, key) {
-    let fragment = template2.content.cloneNode(true);
-    let newBody = document.createElement("body");
-    let item = items[key];
-    let { country, text, imagePath } = item;
-    history.pushState(null, null, `?key=${key}`);
+function renderPage2(countries, countryKey, shallPushState=true) {
+    if (shallPushState) history.pushState(null, null,`?country=${countryKey}`);
+    const country = countries[countryKey];
+    const{name, text, imagePath} = country;
+    const fragment = template2.cloneNode(true).content;
+    const itemImage = fragment.querySelector(".item-side-image");
+    const itemBody = fragment.querySelector(".item-body");
+    const itemTitle = itemBody.querySelector(".item-title");
+    const itemText = itemBody.querySelector(".item-text");
 
-    let img = fragment.getElementById("item-side-image");
-    img.src = imagePath;
-    let h2 = fragment.getElementById("item-title");
-    h2.textContent = country;
-    let p = fragment.getElementById("item-text");
-    p.textContent = text;
+    itemImage.src = imagePath;
+    itemTitle.textContent = name; 
+    itemText.textContent = text;
 
-    body.remove();
-    newBody.appendChild(fragment);
-    bodyParent.appendChild(newBody);
 
-    let goBack = document.getElementById("go-back");
+    page1.replaceChildren(...currentPageElement.children);
+    currentPageElement.replaceChildren(fragment);
+
+    const goBack =  currentPageElement.querySelector("#go-back"); 
+
     goBack.addEventListener("click", e => {
-        renderTemplate1(items);
-        history.pushState(null, null, "index.html");
         e.preventDefault();
+        renderPage1(countries);
     });
-}
-
-function reRenderTemplate1() {
-    let currentBody = bodyParent.getElementsByTagName("body")[0];
-    currentBody.remove();
-    bodyParent.appendChild(body);
+    currentPage = "page2";
 }
